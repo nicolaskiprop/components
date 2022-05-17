ceec7954fee8b3b58e079583fa8a899d6451c22e
nicolskiprop <yegonicholas@gmail.com>
2022-05-17 18:41:10 +0300

    links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
-import logo from '../../assets/Logo.svg'
+import logo from "../../assets/Logo.svg";
 
 function Navbar() {
   return (
@@ -9,6 +9,15 @@ function Navbar() {
       <div className="content__navbar-links">
         <div className="content__navbar-links_logo">
           <img src={logo} alt="logo" />
+        </div>
+        <div className="content__navbar-links_container">
+          <p>
+            <a href="#Home">Home</a>
+            <a href="#whatisthis">what is this?</a>
+            <a href="#Home">Open AI</a>
+            <a href="#Home">Case Studies</a>
+            <a href="#Home">Library</a>
+          </p>
         </div>
       </div>
     </div>
