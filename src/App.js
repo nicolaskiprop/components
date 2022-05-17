510bde95998eee9376db01e622f0e7cf04a72de3
nicolskiprop <yegonicholas@gmail.com>
2022-05-17 18:01:58 +0300

    navbar development

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react' 
+import { Blog, Features, Footer, Header, Possibility, WhatComponent } from './containers'
+import {  Brand, Cta, Navbar } from './components'
+import './App.css'
 
 
 const App = () => {
   return (
     <div>
-        <h1>components</h1>
+       <div className='App'>
+         <div className='gradient__bg'>
+            <Navbar/>
+            <Header />
+         </div>
+         <Brand />
+         <WhatComponent />
+         <Features />
+         <Possibility />
+         <Cta />
+         <Blog />
+         <Footer />
+       </div>
     </div>
   )
 }
